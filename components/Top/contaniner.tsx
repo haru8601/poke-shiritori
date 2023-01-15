943a6f7539ea0bc9caa7148390760d2e6dac6bf5
sekiharuhito <haroot.net@gmail.com>
2023-01-16 01:14:43 +0900

    feat: しりとりロジック実装

--- components/Top/contaniner.tsx
+++ components/Top/contaniner.tsx
@@ -1,10 +1,110 @@
 import { Poke } from "@/types/Poke";
+import { ChangeEvent, useState } from "react";
 import TopPresenter from "./presenter";
 
 type Props = {
   pokedex: Poke[];
+};
+
+export default function Top({ pokedex }: Props) {
+  const [sentPoke, setSentPoke] = useState<string>("");
+  const [pokeErr, setPokeErr] = useState<string>("");
+  const [myPokeList, setMyPokeList] = useState<string[]>([]);
+  const [enermyPokeList, setEnermyPokeList] = useState<string[]>([]);
+  const handleChangePoke = (e: ChangeEvent<HTMLInputElement>) => {
+    setSentPoke(e.target.value);
+  };
+  const handleSubmitPoke = () => {
+    const candidateNameList: string[] = [];
+    /* しりとりになっているかのチェック */
+    if (enermyPokeList.length) {
+      const enermyLastWord = getShiritoriWord(
+        enermyPokeList[enermyPokeList.length - 1]
+      );
+      if (!sentPoke.startsWith(enermyLastWord)) {
+        setPokeErr(`${enermyLastWord}から始まる名前にしてください`);
+        return;
+      }
+    }
+    /* 存在するかチェック */
+    if (!pokedex.find((poke) => poke.name.japanese == sentPoke)) {
+      setPokeErr("存在しないポケモンです");
+      return;
+    }
+
+    setPokeErr("");
+    /* 記録として配列に格納 */
+    const tmpMyPokeList = myPokeList;
+    tmpMyPokeList.push(sentPoke);
+    setMyPokeList(tmpMyPokeList);
+
+    const lastWord = getShiritoriWord(sentPoke);
+    console.log(lastWord);
+    /* ポケ一覧からアンサーの候補を取得 */
+    pokedex.forEach((poke) => {
+      if (poke.name.japanese.startsWith(lastWord)) {
+        candidateNameList.push(poke.name.japanese);
+      }
+    });
+    const tmpEnermyPokeList = enermyPokeList;
+    /* 候補からランダムに選択 */
+    tmpEnermyPokeList.push(
+      candidateNameList[Math.floor(Math.random() * candidateNameList.length)]
+    );
+    setEnermyPokeList(tmpEnermyPokeList);
+    /* 自分のポケリセット */
+    setSentPoke("");
+  };
+  return (
+    <TopPresenter
+      pokedex={pokedex}
+      targetPoke={sentPoke}
+      pokeErr={pokeErr}
+      myPokeList={myPokeList}
+      enermyPokeList={enermyPokeList}
+      onChangePoke={handleChangePoke}
+      onSubmitPoke={handleSubmitPoke}
+    />
+  );
 }
 
-export default function Top({pokedex}: Props) {
-  return <TopPresenter pokedex={pokedex}/>;
+const getShiritoriWord = (pokeName: string): string => {
+  const notLastList = ["ー", "：", "・", "ッ"];
+  const specialCharMap: { [key: string]: string } = {
+    "２": "ツー",
+    Ｚ: "ゼット",
+    "♂": "オス",
+    "♀": "メス",
+    ァ: "ア",
+    ィ: "イ",
+    ゥ: "ウ",
+    ェ: "エ",
+    ォ: "オ",
+    ャ: "ヤ",
+    ュ: "ユ",
+    ョ: "ヨ",
+    ヮ: "ワ",
+  };
+  const specialCharReg = new RegExp(Object.keys(specialCharMap).join("|"), "g");
+  /* 特殊文字の変換処理 */
+  const replacedPokeName = pokeName.replaceAll(
+    specialCharReg,
+    (str) => specialCharMap[str]
+  );
+  for (let i = 0; i < replacedPokeName.length; i++) {
+    const targetChar = replacedPokeName.charAt(replacedPokeName.length - 1 - i);
+    /* 文字がしりとり出来ない文字でなければ */
+    if (!notLastList.includes(targetChar)) {
+      return targetChar.toUpperCase();
+    }
+  }
+  return "";
+};
+
+if (typeof window !== "undefined") {
+  const form = document && document.querySelector("#form");
+  form?.addEventListener("submit", (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+  });
 }
--- components/Top/presenter.tsx
+++ components/Top/presenter.tsx
@@ -1,13 +1,52 @@
-import { ComponentProps } from "react";
+import { ChangeEvent, ComponentProps, Fragment } from "react";
 import Top from "./contaniner";
 
-type Props = ComponentProps<typeof Top>
+type Props = ComponentProps<typeof Top> & {
+  targetPoke: string;
+  pokeErr: string;
+  myPokeList: string[];
+  enermyPokeList: string[];
+  onChangePoke: (e: ChangeEvent<HTMLInputElement>) => void;
+  onSubmitPoke: () => void;
+};
 
-export default function TopPresenter({pokedex}: Props) {
+export default function TopPresenter({
+  pokedex,
+  targetPoke,
+  pokeErr,
+  myPokeList,
+  enermyPokeList,
+  onChangePoke,
+  onSubmitPoke,
+}: Props) {
   return (
-    <div>
-      <h1 className="font-bold">test</h1>
-      <p>{pokedex[12].name.japanese}</p>
+    <div className="m-3">
+      <h1 className="m-3 font-bold text-center">ポケモンしりとり</h1>
+      <div className="text-center m-3">
+        <p className="text-red-400 h-10">{pokeErr}</p>
+        <input
+          value={targetPoke}
+          onChange={onChangePoke}
+          placeholder="ポケモンを入力してください"
+        />
+        <input type="submit" onClick={onSubmitPoke} />
+      </div>
+      <div className="flex place-content-around">
+        <div className="my">
+          {myPokeList
+            .map((myPoke, index) => {
+              return <p key={index}>{myPoke}</p>;
+            })
+            .reverse()}
+        </div>
+        <div>
+          {enermyPokeList
+            .map((enermyPoke, index) => {
+              return <p key={index}>{enermyPoke}</p>;
+            })
+            .reverse()}
+        </div>
+      </div>
     </div>
   );
 }
