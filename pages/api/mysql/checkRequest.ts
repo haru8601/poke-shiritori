aec51671b13b679c815163c2302d3401d6fdf724
sekiharuhito <haroot.net@gmail.com>
2023-02-14 00:39:27 +0900

    feat: 常にres.endする処理テスト

--- pages/api/mysql/checkRequest.ts
+++ pages/api/mysql/checkRequest.ts
@@ -7,13 +7,13 @@ export default function checkRequest(
 ) {
   if (req.method !== method) {
     console.log("method not allowed");
-    res.redirect("/405");
+    // res.redirect("/405");
     return false;
   }
   if (method == "POST" && req.headers["content-type"] !== "application/json") {
     console.log("csrf???");
     console.log(req.headers);
-    res.redirect("/400");
+    // res.redirect("/400");
     return false;
   }
   // if (requestIp.getClientIp(req) !== "::1") {
