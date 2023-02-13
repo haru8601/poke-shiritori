f0c6e1a7bc34e01de909abba8e3171e7b10b7d4d
sekiharuhito <haroot.net@gmail.com>
2023-02-14 03:00:44 +0900

    Revert "feat: ip制限テスト"
    This reverts commit 547179ecf4880f8777c6e5d64c8c9276e8e0e01e.

--- pages/api/mysql/checkRequest.ts
+++ pages/api/mysql/checkRequest.ts
@@ -16,11 +16,5 @@ export default function checkRequest(
     res.redirect("/400").end();
     return false;
   }
-  if (req.headers.host !== "localhost:3000") {
-    console.log("who are u?");
-    console.log(req.headers);
-    res.redirect("/400").end();
-    return false;
-  }
   return true;
 }
