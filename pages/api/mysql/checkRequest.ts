59e6821b97333f17cdb9e3957c2f428ac78c872c
sekiharuhito <haroot.net@gmail.com>
2023-02-14 02:23:30 +0900

    feat: test1

--- pages/api/mysql/checkRequest.ts
+++ pages/api/mysql/checkRequest.ts
@@ -1,5 +1,4 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import requestIp from "request-ip";
 
 export default function checkRequest(
   req: NextApiRequest,
@@ -11,17 +10,17 @@ export default function checkRequest(
     res.redirect("/405").end();
     return false;
   }
-  if (method == "POST" && req.headers["content-type"] !== "application/json") {
-    console.log("csrf???");
-    console.log(req.headers);
-    res.redirect("/400").end();
-    return false;
-  }
-  if (requestIp.getClientIp(req) !== "::1") {
-    console.log("who are u?");
-    console.log(req);
-    res.redirect("/400").end();
-    return false;
-  }
+  // if (method == "POST" && req.headers["content-type"] !== "application/json") {
+  console.log("csrf???");
+  console.log(req.headers);
+  res.redirect("/400").end();
+  //   return false;
+  // }
+  // if (requestIp.getClientIp(req) !== "::1") {
+  //   console.log("who are u?");
+  //   console.log(req);
+  //   res.redirect("/400").end();
+  //   return false;
+  // }
   return true;
 }
