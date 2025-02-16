6546d423a47dbdbdbf4b0b63b19a1d9734cb4fdd
sekiharuhito <haroot.net@gmail.com>
2025-02-16 10:56:11 +0900

    fix: デプロイエラー対応

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
   metadataBase: new URL(
     process.env.NEXT_PUBLIC_SITE_URL ||
       process.env.VERCEL_URL ||
-      "http://localhost:3000"
+      "https://www.pikachu-unown.com/"
   ),
   title: "ポケモンしりとり by haroot",
   description: "harootが作成したポケモンしりとり用サイトです。",
